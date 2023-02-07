fd937cafcb9842faa51831d9493f1595aef140e9
Fidelis <fideliofidel9@gmail.com>
2023-02-07 15:13:34 +0300

    Components rendered

---
+++ src/components/Meme.js
@@ -0,0 +1,83 @@
+import React from 'react'
+
+export default function Meme() {
+// UseState
+    const [meme, setMeme] = React.useState(
+        {
+            topText: "",
+            bottomText: "",
+            randomImage: "http://i.imgflip.com/1bij.jpg"
+        }
+        )
+
+        const [allMemes, setAllMemes] = React.useState([]);
+// get random meme
+        function getMemeImage() {
+            const randomNumber = Math.floor( Math.random()* allMemes.length );
+            const url = allMemes[randomNumber].url;
+
+            setMeme(prevMeme => ({
+                ...prevMeme,
+                randomImage: url
+            }));
+        }
+// Event handler
+        function handleChange(event) {
+            const {name, value} = event.target;
+            setMeme(prevMeme => ({
+                ...prevMeme,
+                [name]: value
+            }));
+        }
+
+// useEffect
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes");
+            const data = await res.json();
+            setAllMemes(data.data.memes);
+        }
+        getMemes();
+    }, []);
+
+// Download button handler
+
+    return (
+        <main className='container'>
+            <div className='form'>
+                <input
+                    type='text'
+                    placeholder='Top text'
+                    className='form--input'
+                    name='topText'
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+
+                <input
+                    type='text'
+                    placeholder='Bottom text'
+                    className='form--input'
+                    name='bottomText'
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
+
+                <button className='form--button' onClick={getMemeImage}>
+                    Get new Meme Image 🖼
+                </button>
+
+                <div className='meme'>
+                    <img
+                        src={meme.randomImage}
+                        className='meme--image'
+                    />
+                    <h2 className='meme--text top'>{meme.topText}</h2>
+                    <h2 className='meme--text bottom'>{meme.bottomText}</h2>
+
+                    <button className='download--btn' onClick={handleDownload}>Download</button>
+                </div>
+            </div>
+        </main>
+    )
+}
