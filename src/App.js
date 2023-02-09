ab4ba1dfaf57cc47397d67fe07019e6a3b78524b
Fidelis <fideliofidel9@gmail.com>
2023-02-09 11:02:05 +0300

    Nav light/dark mode added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React, {useState} from 'react';
 import './App.css';
 import Header from './components/Header';
 import Meme from './components/Meme';
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(true)
+
+  function toggleDarkMode() {
+    setDarkMode(prevMode => !prevMode)
+  }
   return (
     <div className="App">
-      <Header />
-      <Meme />
+      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Meme darkMode={darkMode} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,24 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header(props) {
     return (
-        <header className='header'>
+        <nav className={props.darkMode ? "dark" : ""}>
             <img
                 className='header--image' 
                 src='favicon.png'
                 alt=''
             />
             <h2 className='header--title'>Meme Generator</h2>
-        </header>
+
+            <div className='toggler'>
+                <p className='toggler--light'>Light</p>
+
+                <div className='toggler--slider' onClick={props.toggleDarkMode}>
+                    <div className='toggler--slider--circle'></div>
+                </div>
+
+                <p className='toggler--dark'>Dark</p>
+            </div>
+        </nav>
     )
 }
